91bc82a3be64ad9c03be53726d61474fc14d1ce6
LexKisby <alexander.r.kisby@durham.ac.uk>
2021-09-18 16:35:24 +0100

    running in tkinter window

--- clean.py
+++ clean.py
@@ -1,5 +1,6 @@
 import time
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 import json
 import os
 import ctypes
@@ -15,24 +16,46 @@ def is_admin():
 
 class c_Cleaner():
     def __init__(self):
-        print('starting')
-
+        print('cleaner init')
         pass
 
     def clean(self):
-        os.remove('ggt.txt')
+        print('clean')
         pass
 
     def clean_strict(self):
         pass
 
 
+class Application(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.cleaner = c_Cleaner()
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = ttk.Button(self)
+        self.hi_there["text"] = "Clean"
+        self.hi_there["command"] = self.cleaner.clean
+        self.hi_there.pack(side="top")
+
+        self.quit = ttk.Button(self, text="QUIT",
+                               command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+
 if __name__ == '__main__':
     print(os.listdir())
-    if is_admin():
+    if is_admin() or True:
         # Code of your program here
-        cleaner = c_Cleaner()
-        cleaner.clean()
+        print('starting')
+        root = Tk(className='File Cleaner')
+        root.geometry("280x130")
+        app = Application(master=root)
+        app.mainloop()
+
     else:
         # Re-run the program with admin rights
         ctypes.windll.shell32.ShellExecuteW(
